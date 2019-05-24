1859a4914b7047cda8146dbe0de57977d96989e5
FirmanRiskiPratama <48075406+FirmanRiskiPratama@users.noreply.github.com>
2019-05-24 20:33:10 +0700

    Menampilkan tabel testimoni
    CRUD 90%

---
+++ application/controllers/Testimoni.php
@@ -0,0 +1,56 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Testimoni extends CI_Controller {
+	function __construct(){
+		parent::__construct();
+		$this->load->helper(array('url'));
+		$this->load->model('model_testimoni');
+	}
+	public function index (){
+		$data = array(
+		'data'=>$this->model_testimoni->get_data());
+
+		$this->load->view('app/list_testimoni',$data);
+	}
+	function update(){
+		$id = $this->input->post('nama');
+		//var_dump($id);
+		$insert=$this->model_testimoni->update(array(
+
+				'nama' => $this->input-> post('nama'),
+				'gambar' => $this->input-> post('gambar'),
+				'deskripsi' => $this->input-> post('deskripsi'),
+				
+			), $id);
+		redirect('Testimoni');
+    }
+
+	function delete($id){
+		$this->model_testimoni->delete($id);
+		redirect('Testimoni');
+	}
+	function edit($id){
+		$id = $this->uri->segment(3);
+		//var_dump($id);
+		$data = array (
+			'user' => $this->model_testimoni->get_data_edit($id)
+		);
+		//var_dump($data);
+		
+		$this->load->view("app/edit_testimoni",$data);
+	}
+	
+	function input(){
+		$this->load->view('app/input_testimoni');
+			
+			if (isset($_POST['btnTambah'])){
+			$data = $this->model_testimoni->input(array (
+			'id' => $this->input->post('id'),
+			'nama' => $this->input->post('nama'),
+			'gambar' => $this->input->post('gambar'),
+			'deskripsi' => $this->input->post('deskripsi')));
+			redirect('Testimoni');
+		}
+	}
+}
---
+++ application/models/model_testimoni.php
@@ -0,0 +1,34 @@
+<?php
+class model_testimoni extends CI_Model {
+	
+	function get_table(){
+        return $this->db->get("testimoni");
+    }
+
+    function get_data(){
+		$query = $this->db->query("SELECT * FROM testimoni");
+		return $query->result();
+		//return $query->result_array();
+	}
+
+	function input($data = array()){
+		//query builder
+		return $this->db->insert('testimoni',$data);
+	}
+
+	function delete($id){
+		$this->db->where('id',$id);
+		$this->db->delete('testimoni');
+		
+	}
+
+	function get_data_edit($id){
+		$query = $this->db->query("SELECT * FROM testimoni where id='$id'");
+		return $query->result_array();
+	}
+
+	function update($data=array(),$id){
+		$this->db->where('nama',$id);
+		return $this->db->update('testimoni',$data);
+	}
+}
