92ff9fd5305092d23ffd78c1fb8fec9f3639e4c5
dindamaraida <48000954+dindamaraida@users.noreply.github.com>
2019-05-28 11:09:15 +0700

    Profile
    Menampilkan profile dan CRUD profile

---
+++ application/controllers/Profile.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Profile extends CI_Controller{
+	function __construct(){
+		parent::__construct();
+		$this->load->helper(array('url'));
+		$this->load->model('model_profile');
+
+		// if(! $this->session->logged_in){
+		// 	redirect('loginproses/login');
+		// }
+
+	}
+
+	public function index (){
+		$data = array(
+		'data'=>$this->model_profile->get_data());
+		//var_dump ($data);
+		$this->load->view('app/list_profile',$data);
+
+	}
+}
---
+++ application/models/model_profile.php
@@ -0,0 +1,33 @@
+<?php
+class model_profile extends CI_Model {
+
+	function get_table(){
+		return $this->db->get("profile");
+	} 
+
+	function get_data(){
+		$query = $this->db->query("SELECT * FROM profile");
+		return $query->result();
+		//return $query->result_array();
+	}
+
+	function input($data = array()){
+		//query builder
+		return $this->db->insert('profile', $data);
+	}
+
+	function delete($id){
+		$this->db->where('id', $id);
+		$this->db->delete('profile');
+	}
+
+	function get_data_edit($id){
+		$query = $this->db->query("SELECT * FROM profile where id_profile='$id'");
+		return $query->result_array();
+	}
+
+	function update($data=array(),$id){
+		$this->db->where('judul', $id);
+		return $this->db->update('profile', $data);
+	}
+}
