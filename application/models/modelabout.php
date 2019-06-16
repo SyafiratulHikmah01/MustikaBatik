963d9af7f2f16a5d82d090cdad50705bfccfa269
dindamaraida <48000954+dindamaraida@users.noreply.github.com>
2019-06-16 22:02:49 +0700

    update lagi bos

--- application/models/modelabout.php
+++ application/models/modelabout.php
@@ -5,11 +5,11 @@ class modelabout extends CI_Model {
  //        return $this->db->get("produk");
  //    }
 
- //    function get_data(){
-	// 	$query = $this->db->query("SELECT * FROM home");
-	// 	return $query->result();
-	// 	//return $query->result_array();
-	// }
+    function get_data(){
+		$query = $this->db->query("SELECT * FROM home");
+		return $query->result();
+		return $query->result_array();
+	}
 
 	// function get_data_detail($id){
 	// 	$query = $this->db->query("SELECT * FROM home where id_produk='$id'");
