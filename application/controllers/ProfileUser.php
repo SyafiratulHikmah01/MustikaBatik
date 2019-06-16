35ac749bc3f1e5d44ed16af1525606680576da82
dindamaraida <48000954+dindamaraida@users.noreply.github.com>
2019-06-16 20:58:26 +0700

    update menu about

---
+++ application/controllers/ProfileUser.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class ProfileUser extends CI_Controller {
+
+	function __construct(){
+		parent::__construct();
+		$this->load->helper(array('url'));
+		$this->load->library(array('cart'));
+
+		$this->load->model('modelabout');
+	}
+	
+	public function index()
+	{
+
+		// $data = array(
+		// 'data'=>$this->modelabout->get_data());
+		$data['content'] = 'public/content/c_about';
+		$this->load->view('index', $data);
+	}
+
+}
